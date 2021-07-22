c3e59a72f5f67d898da4e86aa092c8b24ec0c14c
Eddort <webisforce@gmail.com>
2021-07-22 13:16:05 +0300

    wip: strong types

--- utils/get.d.ts
+++ utils/get.d.ts
@@ -1,127 +1,29 @@
-type PropertyPath = string
+/* eslint-disable no-shadow */
+/* eslint-disable @typescript-eslint/array-type */
+export type ResolveType<
+  ObjectType,
+  Path extends string
+> = Path extends keyof ObjectType
+  ? ObjectType[Path]
+  : ObjectType extends Array<infer U>
+  ? Path extends `${number}.${infer RightSide}`
+    ? RightSide extends keyof U
+      ? ResolveType<U, RightSide>
+      : Path extends `${number}.${infer RightSide}.${infer OtherSide}`
+      ? RightSide extends keyof U
+        ? ResolveType<U, `${RightSide}.${OtherSide}`>
+        : 'nope233'
+      : 'nope'
+    : Path extends `${number}`
+    ? U
+    : 'nope22'
+  : Path extends `${infer LeftSide}.${infer RightSide}`
+  ? LeftSide extends keyof ObjectType
+    ? ResolveType<ObjectType[LeftSide], RightSide>
+    : 'notleft'
+  : 'nope2'
 
-interface NumericDictionary<T> {
-  [index: number]: T
-}
-
-export function get<TObject extends object, TKey extends keyof TObject>(
-  object: TObject,
-  path: TKey | [TKey]
-): TObject[TKey]
-
-export function get<TObject extends object, TKey extends keyof TObject>(
-  object: TObject | null | undefined,
-  path: TKey | [TKey]
-): TObject[TKey] | undefined
-
-export function get<TObject extends object, TKey extends keyof TObject>(
-  object: TObject | null | undefined,
-  path: TKey | [TKey]
-): Exclude<TObject[TKey], undefined>
-
-export function get<
-  TObject extends object,
-  TKey1 extends keyof TObject,
-  TKey2 extends keyof TObject[TKey1]
->(object: TObject, path: [TKey1, TKey2]): TObject[TKey1][TKey2]
-
-export function get<
-  TObject extends object,
-  TKey1 extends keyof TObject,
-  TKey2 extends keyof TObject[TKey1]
->(
-  object: TObject | null | undefined,
-  path: [TKey1, TKey2]
-): TObject[TKey1][TKey2] | undefined
-
-export function get<
-  TObject extends object,
-  TKey1 extends keyof TObject,
-  TKey2 extends keyof TObject[TKey1]
->(
-  object: TObject | null | undefined,
-  path: [TKey1, TKey2]
-): Exclude<TObject[TKey1][TKey2], undefined>
-
-export function get<
-  TObject extends object,
-  TKey1 extends keyof TObject,
-  TKey2 extends keyof TObject[TKey1],
-  TKey3 extends keyof TObject[TKey1][TKey2]
->(object: TObject, path: [TKey1, TKey2, TKey3]): TObject[TKey1][TKey2][TKey3]
-
-export function get<
-  TObject extends object,
-  TKey1 extends keyof TObject,
-  TKey2 extends keyof TObject[TKey1],
-  TKey3 extends keyof TObject[TKey1][TKey2]
->(
-  object: TObject | null | undefined,
-  path: [TKey1, TKey2, TKey3]
-): TObject[TKey1][TKey2][TKey3] | undefined
-
-export function get<
-  TObject extends object,
-  TKey1 extends keyof TObject,
-  TKey2 extends keyof TObject[TKey1],
-  TKey3 extends keyof TObject[TKey1][TKey2]
->(
-  object: TObject | null | undefined,
-  path: [TKey1, TKey2, TKey3]
-): Exclude<TObject[TKey1][TKey2][TKey3], undefined>
-
-export function get<
-  TObject extends object,
-  TKey1 extends keyof TObject,
-  TKey2 extends keyof TObject[TKey1],
-  TKey3 extends keyof TObject[TKey1][TKey2],
-  TKey4 extends keyof TObject[TKey1][TKey2][TKey3]
->(
-  object: TObject,
-  path: [TKey1, TKey2, TKey3, TKey4]
-): TObject[TKey1][TKey2][TKey3][TKey4]
-
-export function get<
-  TObject extends object,
-  TKey1 extends keyof TObject,
-  TKey2 extends keyof TObject[TKey1],
-  TKey3 extends keyof TObject[TKey1][TKey2],
-  TKey4 extends keyof TObject[TKey1][TKey2][TKey3]
->(
-  object: TObject | null | undefined,
-  path: [TKey1, TKey2, TKey3, TKey4]
-): TObject[TKey1][TKey2][TKey3][TKey4] | undefined
-
-export function get<
-  TObject extends object,
-  TKey1 extends keyof TObject,
-  TKey2 extends keyof TObject[TKey1],
-  TKey3 extends keyof TObject[TKey1][TKey2],
-  TKey4 extends keyof TObject[TKey1][TKey2][TKey3]
->(
-  object: TObject | null | undefined,
-  path: [TKey1, TKey2, TKey3, TKey4]
-): Exclude<TObject[TKey1][TKey2][TKey3][TKey4], undefined>
-
-export function get<T>(object: NumericDictionary<T>, path: number): T
-
-export function get<T>(
-  object: NumericDictionary<T> | null | undefined,
-  path: number
-): T | undefined
-
-export function get<T>(
-  object: NumericDictionary<T> | null | undefined,
-  path: number
-): T
-
-export function get<T>(
-  object: NumericDictionary<T> | null | undefined,
-  path: number
-): T
-
-export function get(object: null | undefined, path: PropertyPath): undefined
-
-export function get<Returns>(object: any, path: PropertyPath): Returns
-
-export function get(object: any, path: PropertyPath): any
+export function get<ObjecType, Path extends string>(
+  obj: ObjecType,
+  path: Path
+): ResolveType<ObjecType, Path>
--- utils/get.test.ts
+++ utils/get.test.ts
@@ -7,12 +7,12 @@ jest.useFakeTimers()
 describe('get util:', () => {
   it('from nested object', () => {
     expect(get({ a: { b: 1 } }, 'a.b')).toBe(1)
-    get({ kek: { lol: 1 } }, ['kek', 'lol'])
+    get({ kek: { lol: 1 } }, 'kek.lol')
   })
   it('from nested array', () => {
-    expect(get<number>({ a: [1] }, 'a.0')).toBe(1)
+    expect(get({ a: [1] }, 'a.0')).toBe(1)
   })
   it('array as key', () => {
-    expect(get({ a: [1] }, ['a', 0])).toBe(1)
+    expect(get({ a: [1] }, 'a.0')).toBe(1)
   })
 })
