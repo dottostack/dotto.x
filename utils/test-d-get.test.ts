86b563ae1c8938ea60ef2b523acbacd2515c25ea
Eddort <webisforce@gmail.com>
2021-07-23 08:52:58 +0300

    wip: tests resolveType

--- utils/test-d-get.test.ts
+++ utils/test-d-get.test.ts
@@ -39,16 +39,21 @@ const AssertType = <T>(expect: [T] extends [never] ? never : T): T => expect
 describe('type get testing', () => {
   it('1 level obj', () => {
     type Assertion = ResolveType<TestClass, 'arr'>
-    expect(AssertType<Assertion>([1])).toBe([1])
+    expect(AssertType<Assertion>([1])).toEqual([1])
   })
   it('1 level string', () => {
     type Assertion = ResolveType<TestClass, 'normal'>
-    expect(AssertType<Assertion>('sss')).toBe('sss')
+    expect(AssertType<Assertion>('sss')).toEqual('sss')
   })
 
   it('1 level object', () => {
     type Assertion = ResolveType<TestClass, 'deep'>
-    expect(AssertType<Assertion>({ arr: ['s'] })).toBe({ arr: ['s'] })
+    expect(AssertType<Assertion>({ arr: ['s'] })).toEqual({ arr: ['s'] })
+  })
+
+  it('nested object', () => {
+    type Assertion = ResolveType<TestClass, 'nested.b.c'>
+    expect(AssertType<Assertion>(true)).toEqual(true)
   })
 
   it('deep', () => {
@@ -56,6 +61,12 @@ describe('type get testing', () => {
       TestClass,
       'deeplvl1.0.deeplvl2.deeplvl3.0.deeplvl4.deeplvl5.value'
     >
-    expect(AssertType<Assertion>(/ss/)).toBe(/ss/)
+    expect(AssertType<Assertion>(/ss/)).toEqual(/ss/)
+  })
+
+  it('not exists', () => {
+    type Assertion = ResolveType<TestClass, 'deep.dsadsa'>
+    const nope = false as never
+    expect(AssertType<Assertion>(nope)).toEqual(nope)
   })
 })
