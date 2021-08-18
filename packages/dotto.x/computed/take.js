0291674046d40afef7ab70efb890c84963c9397c
Eddort <webisforce@gmail.com>
2021-08-18 14:38:08 +0300

    fix: delete unused packages

--- packages/dotto.x/computed/take.js
+++ packages/dotto.x/computed/take.js
@@ -3,7 +3,7 @@ import { target } from './context'
 export const take = (store, query) => {
   if (!store.set) return store.get(true)
   let container = target()
-  if (container.silent) return store.get(query)
+  if (!container || container.silent) return store.get(query)
   container.add(store, query)
   return store.get(query)
 }
--- packages/dotto.x/index.d.ts
+++ packages/dotto.x/index.d.ts
@@ -4,5 +4,6 @@ export * from './computed'
 export { mount } from './mount'
 
 export { task, allTasks } from './task'
+export { ResolveType } from './utils/get'
 
 export { effect } from './effect'
