767ff405b480494edde4ba7da234701a936d2d0a
Eddort <webisforce@gmail.com>
2021-08-23 09:26:37 +0300

    fix: bind operator with plugins

--- packages/dotto.x/bind/index.js
+++ packages/dotto.x/bind/index.js
@@ -2,6 +2,6 @@ import { computed, take } from '../computed'
 
 export const bind = (store, path) => {
   let bindedStore = computed(() => take(store, path))
-  bindedStore.set = store.set.bind(store, path)
+  bindedStore.set = value => store.set(path, value)
   return bindedStore
 }
--- packages/dotto.x/bind/index.test.ts
+++ packages/dotto.x/bind/index.test.ts
@@ -1,4 +1,5 @@
 import { createStore } from '../create-store'
+import { onSet } from '../lifecycle'
 import { bind } from './index'
 
 describe('bind', () => {
@@ -7,11 +8,26 @@ describe('bind', () => {
     let some = bind(store, 'some')
     expect(some.get()).toBe(1)
   })
+
   it('set', () => {
     let store = createStore({ some: 1 })
     let some = bind(store, 'some')
     expect(some.set(2)).toBe(2)
     expect(some.get()).toBe(2)
     expect(store.get('some')).toBe(2)
+  })
+
+  it('use plugin', () => {
+    let store = createStore({ some: 1 })
+    let events: string[] = []
+    let some = bind(store, 'some')
+
+    onSet(store, ([path]) => {
+      events.push(path as string)
+    })
+
+    some.set(2)
+    some.set(3)
+    expect(events).toEqual(['some', 'some'])
   })
 })
