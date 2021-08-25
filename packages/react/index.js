6cf5334ef6a1e04edced9264d5760d4117c035fb
Eddort <webisforce@gmail.com>
2021-08-25 13:06:28 +0300

    feat: react binding to new api

--- packages/react/index.js
+++ packages/react/index.js
@@ -5,7 +5,7 @@ export const useStore = (store, selector) => {
   let [, force] = useState({})
   useEffect(() => {
     let cb = () => unstable_batchedUpdates(() => force({}))
-    return store._run ? store.listen(cb) : store.listen(selector, cb)
+    return store._run ? store.listen(cb) : store.watch(selector, cb)
   }, [selector, store])
   return store.get(selector)
 }
