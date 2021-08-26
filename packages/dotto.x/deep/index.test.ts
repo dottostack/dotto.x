c7def150a1125ab087ee9ce555513d9b3b3f6a48
Eddort <webisforce@gmail.com>
2021-08-26 17:39:22 +0300

    wip: tests

--- packages/dotto.x/deep/index.test.ts
+++ packages/dotto.x/deep/index.test.ts
@@ -1,7 +1,7 @@
 import { createStore } from '../create-store'
 import { deep } from './index'
 
-it('watch-deep', () => {
+it('deep', () => {
   type Deep = {
     some: {
       deep: number
@@ -27,6 +27,36 @@ it('watch-deep', () => {
     { some: { deep: 1 } },
     { some: { deep: 1, test: 1 } },
     { some: { deep: 1, test: 3 } }
+  ])
+  unsub()
+})
+
+it('deep store off', () => {
+  type Deep = {
+    some: {
+      deep: number
+      test?: number
+    }
+  }
+
+  let data: Deep[] = []
+
+  let store = createStore<Deep>({ some: { deep: 1 } })
+
+  let unsub = deep(store).listen(value => {
+    data.push(JSON.parse(JSON.stringify(value)))
+  })
+
+  store.set('some', { deep: 2 })
+  store.set('', { some: { deep: 1 } })
+  store.set('', { some: { deep: 1, test: 1 } })
+  store.off()
+  store.set('some.test', 3)
+
+  expect(data).toEqual([
+    { some: { deep: 2 } },
+    { some: { deep: 1 } },
+    { some: { deep: 1, test: 1 } }
   ])
   unsub()
 })
