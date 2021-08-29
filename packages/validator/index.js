513aa4e83d733d80752dfb0a225071c379baac99
Eddort <webisforce@gmail.com>
2021-08-29 19:01:24 +0300

    wip: refactor validator plugin

--- packages/validator/index.js
+++ packages/validator/index.js
@@ -1,29 +1,22 @@
-import { computed, createStore, deep, take } from 'dotto.x'
-import { onSet } from 'dotto.x/lifecycle'
+import { computed, createAtom, take } from 'dotto.x'
+import { onChange } from 'dotto.x/lifecycle'
+import { get } from 'dotto.x/utils/get'
 
-export const validator = (store, rules, config = {}) => {
-  let errorsStore = createStore({})
-  let errors = deep(errorsStore)
+export const validator = (store, rules) => {
+  let errors = createAtom({})
   let valid = computed(() => !Object.values(take(errors)).filter(a => a).length)
-  let container = rules.reduce(
-    (acc, { path, validators }) => ({ ...acc, [path]: validators }),
-    {}
-  )
-  let destroy = onSet(store, ([path, value], { methods }) => {
-    let target = container[path]
-    if (!target) return
-    let reason
-    let hasError = target.some(rule => {
-      let res = rule(value, methods)
-      if (res) reason = res
-      return res
-    })
 
-    errorsStore.set(path, reason)
-    if (hasError && config.abort) {
-      methods.abort()
+  let destroy = onChange(store, (_, { methods }) => {
+    for (let { path, validators } of rules) {
+      let target = get(store.get(), path)
+      let reason
+      validators.some(rule => {
+        let res = rule(target, methods)
+        if (res) reason = res
+        return res
+      })
+      errors.set({ ...errors.get(), [path]: reason })
     }
   })
-
   return { errors, destroy, valid }
 }
