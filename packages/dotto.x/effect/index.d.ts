46545a92eb440a90b57621149f40ad7ae345aa4e
Eddort <webisforce@gmail.com>
2021-08-29 10:43:27 +0300

    feat: update operator

--- packages/dotto.x/effect/index.d.ts
+++ packages/dotto.x/effect/index.d.ts
@@ -1,7 +1,8 @@
-import { DotXStore } from '../create-store'
-import { ReadableStore } from '../computed'
+import type { DotXStore } from '../create-store'
+import type { ReadableStore } from '../computed'
+import type { DotXAtom } from '../create-atom'
 
-type EffectDep<Data> = DotXStore<Data> | ReadableStore<Data>
+type EffectDep<Data> = DotXStore<Data> | ReadableStore<Data> | DotXAtom
 
 export function effect(store: EffectDep<any>, cb: () => void): () => void
 
